8bf60c0aa26c2add781933bc6407ad3510599bd6
alitvin <litwinoff@gmail.com>
2017-02-03 13:16:57 +0300

    Retrieving Objects using session.get

--- IntroHibernate/FirstHibernateProj/src/main/java/org/koushnik/hibernate/HibernateTest.java
+++ IntroHibernate/FirstHibernateProj/src/main/java/org/koushnik/hibernate/HibernateTest.java
@@ -23,8 +23,8 @@ public class HibernateTest {
 		user.setDescription("Description of the user goes here");
 		
 		
-		// now we need persist the object to the DB
-        // let's use Hibernate API without DAO!
+		/* now we need persist the object to the DB
+           let's use Hibernate API without DAO! */
 		
 		// reading configuration file and creating a session factory		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -41,7 +41,20 @@ public class HibernateTest {
 		// after saving we will have to end the transaction
 		session.getTransaction().commit();
 		
+		// closing session
+		session.close();
 		
+		/* Let's fetch user from DB now */
+		
+		user = null;
+		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		// fetching: specify class and what data do we need,
+		// Primary Key field usually, e.g. =1 here	
+		user = (UserDetails) session.get(UserDetails.class, 1);
+		System.out.println("User Name retrieved is " + user.getUserName());
 	}
 
 }
