11e24b26e5c8b6c9625ad8d1738a96c6df1ec267
alitvin <litwinoff@gmail.com>
2017-02-03 12:15:25 +0300

    Can annotatating getters instead of fields

--- IntroHibernate/FirstHibernateProj/src/main/java/org/javabrains/koushnik/dto/UserDetails.java
+++ IntroHibernate/FirstHibernateProj/src/main/java/org/javabrains/koushnik/dto/UserDetails.java
@@ -8,22 +8,25 @@ import javax.persistence.Id;
 @Entity (name="USER_DETAILS")
 public class UserDetails {
 
+	private int userId;	
+	private String userName;
+
+	// We can place annots on getters instead!
+	// so Hibernate picks up the value from getter 
+	
 	// @Id used for Primary Key 
 	@Id
 	@Column(name="USER_ID")
-	private int userId;
-	
-	@Column(name="USER_NAME")
-	private String userName;
-	
 	public int getUserId() {
 		return userId;
 	}
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+	
+	@Column(name="USER_NAME")
 	public String getUserName() {
-		return userName;
+		return userName + " from getter";
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
