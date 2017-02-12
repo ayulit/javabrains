68e9dc92625ae1698ec5923b5738c4d3e14154d4
ayulit <litwinoff@gmail.com>
2017-02-12 15:41:58 +0300

    Just simple HelloWorld

---
+++ Java8lambdaBasics/src/main/java/io/javabrains/Greeter.java
@@ -0,0 +1,15 @@
+package io.javabrains;
+
+public class Greeter {
+
+	public void greet() {
+		System.out.println("Hello world!");
+	}
+	
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		greeter.greet();
+
+	}
+
+}
