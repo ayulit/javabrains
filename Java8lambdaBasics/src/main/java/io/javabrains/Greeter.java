5e9a55473ed73e1c4879f666ecf2cb520d862a6c
ayulit <litwinoff@gmail.com>
2017-02-12 15:48:57 +0300

    Classics: passing behavior to greet() method as an IF's impl

--- Java8lambdaBasics/src/main/java/io/javabrains/Greeter.java
+++ Java8lambdaBasics/src/main/java/io/javabrains/Greeter.java
@@ -2,13 +2,16 @@ package io.javabrains;
 
 public class Greeter {
 
-	public void greet() {
-		System.out.println("Hello world!");
+	public void greet(Greeting greeting) {
+		greeting.perform();
 	}
 	
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
-		greeter.greet();
+		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting(); 
+		
+		// passing behavior
+		greeter.greet(helloWorldGreeting);
 
 	}
 
