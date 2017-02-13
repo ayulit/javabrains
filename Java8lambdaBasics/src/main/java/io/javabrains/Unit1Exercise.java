7ac84e741091e4269ffb1075b9038b3e5a09ecb8
alitvin <litwinoff@gmail.com>
2017-02-13 17:39:14 +0300

    Task

---
+++ Java8lambdaBasics/src/main/java/io/javabrains/Unit1Exercise.java
@@ -0,0 +1,26 @@
+package io.javabrains;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise {
+
+	public static void main(String[] args) {
+		
+		List<Person> people = Arrays.asList(
+					new Person("Luke","Skywalker", 25),
+					new Person("Han","Solo", 35),
+					new Person("Lea","Organa", 25),
+					new Person("Darh","Vader", 50),
+					new Person("Emperor","Palpatine", 75)
+				);
+		
+		// Step 1: Sort list by last name
+		
+		// Step 2: Create a method that prints all elements in the list
+		
+		// Step 3: Create a method that prints all people that have last name beginning with C
+
+	}
+
+}
