764526dadd55af8abbcb295094cf7fd5c8cca8a0
alitvin <litwinoff@gmail.com>
2017-03-06 12:52:39 +0300

    2.11 Implementing Update and Delete

--- messenger/src/main/java/org/koushik/javabrains/resources/MessageResource.java
+++ messenger/src/main/java/org/koushik/javabrains/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.koushik.javabrains.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,12 +16,13 @@ import org.koushik.javabrains.model.Message;
 import org.koushik.javabrains.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON) // to convert input JSON to Message 
+@Produces(MediaType.APPLICATION_JSON) // annotating class allows us not to annot methods
 public class MessageResource {
 		
 	MessageService messageService = new MessageService();
 	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@GET	
 	public List<Message> getMessages() {
 		
 		/* we need to convert List to XML using JAXB */
@@ -28,19 +31,30 @@ public class MessageResource {
 	}
 	
 	@GET
-	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{messageId}")	
 	public Message getMessage(@PathParam("messageId") long id) {
 		
 		return messageService.getMessage(id);
 		
 	}
-	
+
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON) // to convert input JSON to Message
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
+	}	
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id); // we need to do it here
+		return messageService.updateMessage(message);
 	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+
 
 }
