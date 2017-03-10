7d7ffaa9a4ffa192bfd96478dcd5dddc3e7eb415
alitvin <litwinoff@gmail.com>
2017-03-10 18:51:06 +0300

    2.18 Handling Exception: JSON response with error

--- messenger/src/main/java/org/koushik/javabrains/exception/DataNotFoundExceptionMapper.java
+++ messenger/src/main/java/org/koushik/javabrains/exception/DataNotFoundExceptionMapper.java
@@ -5,14 +5,23 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.koushik.javabrains.model.ErrorMessage;
+
 /* Special mapper class for mapping DataNotFoundException to response
  * using ErrorMessage model */
 @Provider // this annot registers us in JAX-RS, so it will know, that mapper is here ot map an exception
 public class DataNotFoundExceptionMapper implements ExceptionMapper<DataNotFoundException> {
 
 	@Override
-	public Response toResponse(DataNotFoundException exception) {		
-		return Response.status(Status.NOT_FOUND).build();
+	public Response toResponse(DataNotFoundException ex) {
+		
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(),
+				                                     404, 
+				                                     "http://www.google.com");
+		
+		return Response.status(Status.NOT_FOUND)
+				.entity(errorMessage)
+				.build();
 	}
 
 }
