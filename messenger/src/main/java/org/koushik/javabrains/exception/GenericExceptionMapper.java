9e87404a8df264ca05ef3f38ce15bacd825e43dc
alitvin <litwinoff@gmail.com>
2017-03-10 19:04:02 +0300

    2.18 Handling Exception: Universal Exception handling

---
+++ messenger/src/main/java/org/koushik/javabrains/exception/GenericExceptionMapper.java
@@ -0,0 +1,27 @@
+package org.koushik.javabrains.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.koushik.javabrains.model.ErrorMessage;
+
+
+// Handles ALL exceptions because of Throwable
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable ex) {
+		
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(),
+				                                     500, 
+				                                     "http://www.google.com");
+		
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+				.entity(errorMessage)
+				.build();
+	}
+
+}
